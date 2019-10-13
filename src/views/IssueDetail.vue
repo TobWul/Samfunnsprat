8e7e67782e09cd61cca479650ebb68302bdd4599
Tobias Wulvik <tobias.wulvik@gmail.com>
2019-10-13 17:57:05 +0200

    adding analytics

--- src/views/IssueDetail.vue
+++ src/views/IssueDetail.vue
@@ -10,6 +10,16 @@
     <h3>Hvordan</h3>
     <p>{{ issue.hvordan }}</p>
     <Tweet id="1182584398459355138"></Tweet>
+    <iframe
+      src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FStudenttinget%2Fposts%2F10162370772740607&width=500"
+      width="500"
+      height="626"
+      style="border:none;overflow:hidden"
+      scrolling="no"
+      frameborder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    ></iframe>
   </the-container>
 </template>
 
